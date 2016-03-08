7ecb94dc7eae64f0f49648c1a4146b6ce6e1976d
rabensm <marty@rabens.com>
2016-03-08 15:23:20 -0500

    G-code generation for Path

--- src/base/Config.java
+++ src/base/Config.java
@@ -5,8 +5,14 @@ package base;
  */
 public class Config {
     // depth of each pass when cutting a path
-    static final public double PassDepth = 0.6f;
+    static final public double PassDepth = 0.6;
 
     // g-code feed rate
     static final public double FeedRate = 250;
+
+    // the height to move above the work surface for rapid positioning (G00)
+    static final public double SafeHeight = 1.5;
+
+    // the height above the surface to start feed cut
+    static final public double StartHeight = 0.2;
 }
--- src/base/path/Path.java
+++ src/base/path/Path.java
@@ -1,5 +1,7 @@
 package base.path;
 
+import base.Config;
+
 import java.util.ArrayList;
 import java.util.List;
 import javax.vecmath.Vector3d;
@@ -43,4 +45,40 @@ public class Path {
         return nodes.size();
     }
 
+    /**
+     * Generates the g-code to cut this path
+     *
+     * @return
+     */
+    public String getGCode () {
+        if (nodes.size() < 2) {
+            return "";
+        }
+
+        StringBuilder gCodeStr = new StringBuilder();
+
+        Vector3d firstNode = nodes.get(0);
+
+        // enter rapid move mode and move up to safe height
+        gCodeStr.append("G00 Z").append(Config.SafeHeight).append("\n")
+
+                // move over to above first node
+                .append("X ").append(firstNode.x).append(" Y").append(firstNode.y).append(" Z").append(Config.SafeHeight).append("\n")
+
+                // move down to starting height
+                .append("Z ").append(Config.StartHeight).append("\n")
+
+                // enter linear move mode, and set feed rate
+                .append("G01 F").append(Config.FeedRate).append("\n");
+
+        // append the x, y, z of each of the nodes
+        for (Vector3d node : nodes) {
+            gCodeStr.append("X ").append(node.x).append("Y ").append(node.y).append("Z ").append(node.z).append("\n");
+        }
+
+        // enter rapid move mode and go back to safe height
+        gCodeStr.append("G00 Z").append(Config.SafeHeight).append("\n");
+
+        return gCodeStr.toString();
+    }
 }
